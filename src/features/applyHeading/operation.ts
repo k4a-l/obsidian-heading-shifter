c23109147230eaafe1cab25b0bdacfdafa2c8cbf
kasahala <kento.iku@gmail.com>
2022-08-03 13:37:29 +0900

    refactor: editorの変更をtransactionに統一

--- src/features/applyHeading/operation.ts
+++ src/features/applyHeading/operation.ts
@@ -1,20 +1,22 @@
-import { Editor, MarkdownView } from "obsidian";
+import { Editor, EditorChange, MarkdownView } from "obsidian";
 import { HeadingShifterSettings } from "settings";
 import { applyHeading } from "./module";
 
-const dispatchApplyHeading = (
+const composeApplyHeadingChanges = (
 	editor: Editor,
 	view: MarkdownView,
 	heading: number
-) => {
+): EditorChange[] => {
 	const line = editor.getCursor().line;
 	const lineContent = editor.getLine(line);
 
-	editor.replaceRange(
-		applyHeading(lineContent, heading),
-		{ line, ch: 0 },
-		{ line, ch: lineContent.length }
-	);
+	return [
+		{
+			text: applyHeading(lineContent, heading),
+			from: { line, ch: 0 },
+			to: { line, ch: lineContent.length },
+		},
+	];
 };
 
 export const createApplyHeadingCommand = (
@@ -28,7 +30,10 @@ export const createApplyHeadingCommand = (
 			if (editor.getCursor("from").line! != editor.getCursor("to").line) {
 				return;
 			}
-			dispatchApplyHeading(editor, view, heading);
+
+			editor.transaction({
+				changes: composeApplyHeadingChanges(editor, view, heading),
+			});
 		},
 	};
 };
